1c270bea45a32cd7b01bf38f094c76deeec604ea
shubham_97 <shubham.agarwal@clix.capital>
2021-03-07 01:12:56 +0530

    custom footer added

---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+import styles from "../../styles/Home.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>Developed by Shubham Agarwal </footer>
+  );
+}
