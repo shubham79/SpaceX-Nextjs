9eb0aedbaebc7f942f398b35ac60cbd0f382cfac
shubham_97 <shubham.agarwal@clix.capital>
2021-03-07 06:46:26 +0530

    text update

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,8 @@ import styles from "../../styles/Home.module.css";
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}>Developed by Shubham Agarwal </footer>
+    <footer className={styles.footer} style={{ fontWeight: "bold" }}>
+      Developed by Shubham Agarwal{" "}
+    </footer>
   );
 }
